e337b741a422ff5a8b9c7b3ad84e68a15d732957
rodrigorvix <rodrigovitoriense@gmail.com>
2022-04-06 19:58:29 -0300

    Initial commit

---
+++ src/main/java/br/com/letscode/ecommerce/ECommerceApplication.java
@@ -0,0 +1,15 @@
+package br.com.letscode.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
