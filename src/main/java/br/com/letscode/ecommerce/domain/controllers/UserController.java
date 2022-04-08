1d7f797f9170cca571848e8e22233ab5deb71e56
rodrigorvix <rodrigovitoriense@gmail.com>
2022-04-08 09:42:28 -0300

    Update UserController.

--- src/main/java/br/com/letscode/ecommerce/domain/controllers/UserController.java
+++ src/main/java/br/com/letscode/ecommerce/domain/controllers/UserController.java
@@ -2,12 +2,16 @@ package br.com.letscode.ecommerce.domain.controllers;
 
 import br.com.letscode.ecommerce.domain.models.UserEntity;
 import br.com.letscode.ecommerce.domain.repositories.UserRepository;
+import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/api/users")
 @RestController
@@ -20,23 +24,43 @@ public class UserController {
     }
 
     @GetMapping()
-    public ResponseEntity index() {
+    public List<UserEntity> index() {
         List<UserEntity> users = this.userRepository.findAll();
 
-        return ResponseEntity.ok(users);
+        return users;
     }
 
     @PostMapping("/add")
-    public ResponseEntity store (@RequestBody UserEntity user){
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserEntity store (@RequestBody UserEntity user){
 
         user.setCreatedAt(ZonedDateTime.now());
         user.setUpdatedAt(ZonedDateTime.now());
 
-        System.out.println(user);
+        return  this.userRepository.save(user);
+    }
 
-        UserEntity userSaved = this.userRepository.save(user);
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+  /*  public void delete(@PathVariable Long id) {
 
-        return  ResponseEntity.ok(userSaved);
+        this.userRepository
+                .findById(id)
+                .map( userExists -> this.userRepository.delete(userExists))
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+    }*/
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity update(@RequestBody UserEntity user, @PathVariable Long id) {
+
+       return  this.userRepository
+                .findById(id)
+               .map(userExists -> {
+                   user.setId(userExists.getId());
+                   this.userRepository.save(user);
+                   return ResponseEntity.noContent().build();
+               }).orElseGet( () -> ResponseEntity.notFound().build() );
+
     }
 
 
