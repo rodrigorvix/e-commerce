d47ce79c691a12374da2c18e3cf91a11c4f31798
rodrigorvix <rodrigovitoriense@gmail.com>
2022-04-12 11:15:39 -0300

    Verification if user exists.

--- src/main/java/br/com/letscode/ecommerce/domain/services/UserService.java
+++ src/main/java/br/com/letscode/ecommerce/domain/services/UserService.java
@@ -2,9 +2,12 @@ package br.com.letscode.ecommerce.domain.services;
 
 import br.com.letscode.ecommerce.domain.models.UserEntity;
 import br.com.letscode.ecommerce.domain.repositories.UserRepository;
+import br.com.letscode.ecommerce.exception.RolesException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,10 +17,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,10 +31,6 @@ public class UserService implements UserDetailsService {
 
     private final PasswordEncoder passwordEncoder;
 
- /*   public UserService(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }*/
-
 
     public List<UserEntity> getUsers() {
         List<UserEntity> users = this.userRepository.findAll();
@@ -40,6 +39,18 @@ public class UserService implements UserDetailsService {
     }
 
     public UserEntity createUser(UserEntity user) {
+
+
+       List<UserEntity> usernameSearch = this.userRepository.findAll()
+               .stream()
+               .filter(userExists -> userExists.getUsername().equals(user.getUsername()))
+               .collect(Collectors.toList());
+
+        System.out.println(usernameSearch.size());
+
+       if(usernameSearch.size() > 0) {
+           throw  new RolesException("Usuário já existe.");
+       }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setCreatedAt(ZonedDateTime.now());
         user.setUpdatedAt(ZonedDateTime.now());
